ee2afb8ab63e0907189d3ee380c0f5d89c11eb92
Aryan Verma <amv190301@gmail.com>
2022-09-14 15:19:18 +0530

    delete redundant metric sessions_by_page_path & add missing metric monthly_sessions_by_referrer

--- models/metrics/number_of_sessions/monthly/segment__monthly_number_of_sessions_by_referrer_source.sql
+++ models/metrics/number_of_sessions/monthly/segment__monthly_number_of_sessions_by_referrer_source.sql
@@ -7,8 +7,8 @@
 
 select *
 from {{ metrics.metric(
-    metric_name='segment__daily_number_of_sessions_by_page_path',
-    grain='day',
-    dimensions=['last_page_url_path'],
+    metric_name='segment__monthly_number_of_sessions_by_referrer_source',
+    grain='month',
+    dimensions=['referrer_source'],
     secondary_calculations=[]
 ) }}
